4f8fdfdf1a206ae71fe880e6f2961ec984458be1
Jaime Carvajal <jcarvajalg@psl.com.co>
2019-04-12 00:21:49 -0500

    adding toast message

--- src/app/components/logout/logout.module.ts
+++ src/app/components/logout/logout.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { LogoutPage } from './logout.page';
+import { ToastModule } from "../../modules/toast/toast.module";
 
 const routes: Routes = [
   {
@@ -19,6 +20,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    ToastModule,
     RouterModule.forChild(routes)
   ],
   declarations: [LogoutPage]
