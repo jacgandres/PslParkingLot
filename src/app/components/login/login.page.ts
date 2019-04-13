5ee47930048791ed160865fc4dde4870c538807c
Jaime Carvajal <jcarvajalg@psl.com.co>
2019-04-13 16:13:52 -0500

    Fixing localhost exit bug

--- src/app/components/login/login.page.ts
+++ src/app/components/login/login.page.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthServiceService, DbServiceService } from "../../services/export-services";
-import { Router, ActivatedRoute } from "@angular/router"; 
+import { Router, ActivatedRoute } from "@angular/router";
 import { isNullOrUndefined } from 'util';
-import { FormGroup, FormControl, Validators } from '@angular/forms'; 
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ToastModule } from "../../modules/toast/toast.module";
 import { CommonMethodsModule } from 'src/app/modules/common-methods/common-methods.module';
 import { Platform } from '@ionic/angular';
@@ -17,8 +17,8 @@ export class LoginPage implements OnInit {
   public formgroup: FormGroup;
 
 
-  constructor(private router: Router,private route: ActivatedRoute, private authService: AuthServiceService,  private platform: Platform,
-              private dbLocalService: DbServiceService, private toast:ToastModule, private commonMethods: CommonMethodsModule) {
+  constructor(private router: Router, private route: ActivatedRoute, private authService: AuthServiceService, private platform: Platform,
+    private dbLocalService: DbServiceService, private toast: ToastModule, private commonMethods: CommonMethodsModule) {
 
     var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     this.formgroup = new FormGroup({
@@ -31,25 +31,29 @@ export class LoginPage implements OnInit {
     });
 
     this.route.queryParams.subscribe(params => {
-      this.commonMethods.ConsoleLog("this.route.queryParams" , params);
+      this.commonMethods.ConsoleLog("this.route.queryParams", params);
       if (params && params.special) {
         let parameter = JSON.parse(params.special);
-        if(parameter.CleanUser == true)
-        {
-           dbLocalService.CleanUser();
-           this.exitApp();
+        if (parameter.CleanUser == true) {
+          dbLocalService.CleanUser();
+          this.exitApp();
         }
       }
     });
   }
 
-  exitApp(){
-    navigator['app'].exitApp();
+  exitApp() {
+    if (this.platform.is('cordova')) { 
+      navigator['app'].exitApp();
+    }
+    else {
+      this.commonMethods.ConsoleLog("ha terminado session correactamente", {})
+    }
   }
 
   ngOnInit() {
     this.dbLocalService.GetUser().then(result => {
-      this.commonMethods.ConsoleLog("ngOnInit this.dbLocalService.GetUser()" , result);
+      this.commonMethods.ConsoleLog("ngOnInit this.dbLocalService.GetUser()", result);
       if (!isNullOrUndefined(result)) {
         this.formgroup.controls['loginDetails'].get("Email").setValue(result.Email);
         this.formgroup.controls['loginDetails'].get("Password").setValue(result.Pwd);
@@ -59,9 +63,9 @@ export class LoginPage implements OnInit {
 
   OnSubmitLogin() {
 
-    this.commonMethods.ConsoleLog("OnSubmitLogin" , {});
+    this.commonMethods.ConsoleLog("OnSubmitLogin", {});
     this.authService.LogIn(this.formgroup.value.loginDetails.Email, this.formgroup.value.loginDetails.Password).then(resp => {
-       
+
       this.toast.presentToast(resp.FirstName + " " + resp.LastName + ", has ingresado correctamente.");
       this.router.navigate(['home']);
     }).catch(err => { alert("Datos incorrectos o no existe el usuario") });
@@ -70,5 +74,5 @@ export class LoginPage implements OnInit {
   Register() {
     this.router.navigate(['register']);
   }
- 
+
 }
